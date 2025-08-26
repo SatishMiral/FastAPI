f2b8cc2a1e0f84c10e46ecdebcec7a5a84b13c69
SatishMiral <satishmiral01@gmail.com>
2025-08-26 15:18:39 +0530

    Add Response Model

--- blog/main.py
+++ blog/main.py
@@ -3,6 +3,7 @@ from . import schemas
 from . import models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from typing import List
 
 app = FastAPI()
 
@@ -23,12 +24,12 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog 
 
-@app.get("/blog")
+@app.get("/blog", response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get("/blog/{id}")
+@app.get("/blog/{id}", response_model=schemas.ShowBlog)
 def show(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
